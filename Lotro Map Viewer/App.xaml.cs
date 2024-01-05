51ba7d6021216687079a0d19520a1bd1c69f413a
aya-po <extraordinarilygrey@gmail.com>
2024-01-05 15:23:15 -0500

    Add project files.

---
+++ Lotro Map Viewer/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Lotro_Map_Viewer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Lotro Map Viewer/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace Lotro_Map_Viewer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            XmlDocument doc = new XmlDocument();
+            using (FileStream s = new FileStream("data/regions.xml", FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                doc.Load(s);
+            }
+
+            LoadData.Document = doc;
+
+        }
+        private void TreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (e.NewValue is not null)
+            {
+                var tmp = (XmlElement)e.NewValue;
+                txtNorth.Text = "Not Null";
+                //txtNorth.Text = tmp.InnerXml;
+            }
+            else
+            {
+                txtNorth.Text = "Null";
+            }
+        }
+    }
+}
